f14c464e4bb4f02decea9a4469d53ce445a4b23d
MAsfandYar1995 <muhammadasfandyar12@gmail.com>
2026-05-15 16:51:53 +0100

    Create proc_load_Bronze.sql

---
+++ scripts/Bronze/proc_load_Bronze.sql
@@ -0,0 +1,65 @@
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+
+SELECT * FROM bronze.crm_cust_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT * FROM bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+
+SELECT * FROM bronze.crm_sales_details
+
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT * FROM bronze.erp_cust_az12;
+
+
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT * FROM bronze.erp_loc_a101;
+
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\malih\Downloads\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+SELECT * FROM  bronze.erp_px_cat_g1v2;
